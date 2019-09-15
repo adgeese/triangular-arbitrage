9ed5ab0230865eb8539f8a38f10f3c09bec4b356
yangfan1995725 <570257909@qq.com>
2019-09-15 23:00:50 +0800

    push2

--- config.js
+++ config.js
@@ -8,13 +8,16 @@ config.fee = 0.002 * 0.75
 // 套利交易条件
 config.TradeThreshold = 1.0006
 // 交易币种
-config.coinA = ''
-config.coinB = ''
-config.coinC = ''
+config.coinA = 'BTC/USDT'
+config.coinB = 'BTC/DKKT'
+config.coinC = 'USDT/DKKT'
 // 溢价
 config.coinAPremium = 1.000
 config.coinBPremium = 1.000
 config.coinCPremium = 1.002
+//最大交易价格 USDT
+config.maxPrice = 2
+
 
 
 
--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 "use strict";
 const config = require('./config');
 
-const ccxt = require ('ccxt')
+const ccxt = require ('ccxt');
 
-let exchange = new ccxt[config.exchange]()
-console.log(exchange)
+let exchange = new ccxt[config.exchange]();
+
+let getCoinOrder = 0;  // 0获取三个币种全部买卖单 1，2，3 获取对应币种的数据
+
+// 只返回买一卖一
+let fetchOrderBook = async (coin) => {
+    // 如果报错 则重新获取数据 超过10次 全部数据重新获取
+    try
+    {
+//在此运行代码
+    }
+    catch(err)
+    {
+//在此处理错误
+    }
+    let orderBook = await exchange.fetchOrderBook(coin);
+    return {
+        'bids': orderBook['bids'][0],
+        'asks': orderBook['asks'][0]
+    }
+}
+
+(async function main () {
+    let coinAOrderBook,coinBOrderBook,coinCOrderBook;
+    let book = {}
+    //无限循环
+    while (1===1){
+        if(getCoinOrder === 0){  // 
+            coinAOrderBook = await fetchOrderBook(config.coinA);
+            coinBOrderBook = await fetchOrderBook(config.coinB);
+            coinCOrderBook = await fetchOrderBook(config.coinC);
+            console.log(getCoinOrder)
+            getCoinOrder += 1
+        }else {
+            if(getCoinOrder===1){
+                coinAOrderBook = await fetchOrderBook(config.coinA)
+                console.log(getCoinOrder)
+            }
+            if(getCoinOrder===2){
+                coinBOrderBook = await fetchOrderBook(config.coinB)
+                console.log(getCoinOrder)
+            }
+            if(getCoinOrder===3){
+                coinCOrderBook = await fetchOrderBook(config.coinC);
+                console.log(getCoinOrder)
+                getCoinOrder = 0  // 等于3 重新变成1 后面会 +1 
+            }
+            getCoinOrder += 1
+        }
+
+        // console.log(coinAOrderBook)
+        // console.log(coinBOrderBook)
+        // console.log(coinCOrderBook)
+    }
+    
+})()
